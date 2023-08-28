36a35012324abb15c38dabcd3d08e1fc0bce2155
Dilshodbek <dilshodbeknekmurodov@gmail.com>
2023-08-28 17:44:45 +0500

    first commit

---
+++ src/test/java/com/example/loadpaymentservice/LoadPaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loadpaymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoadPaymentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
